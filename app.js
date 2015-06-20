6b4db39579afe944e80c64f7c992a41cadb8b23d
Javier Llinares <javier.jose.llinares@gmail.com>
2015-06-20 15:42:30 -0430

    Cambios Proyecto
    Primera pagina e inclusion del favicon.

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var express = require('express');
+var router = express.Router();
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -15,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +53,8 @@ app.use(function(err, req, res, next) {
     });
 });
 
+router.get('/', function(req, res){
+	res.render('index', {tittle: 'Quiz'});
+});
 
 module.exports = app;
