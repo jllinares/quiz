f0cb366c37a384d1bcc62e6bf783960364968be6
Javier Llinares <javier.jose.llinares@gmail.com>
2015-06-24 15:59:41 -0430

    Implementacion de Autoload
    Se implementan los cambios para el autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,28 +1,42 @@
 var models = require('../models/models.js');
 
+/* Autoload */
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.findById(quizId).then(function(quiz) {
+		if(quiz) {
+			req.quiz = quiz;
+			next();
+		}
+		
+		else {
+			next(new Error("No existe el quizId=" + quizId));
+		}
+	}).catch(function(error) { 
+		next(error);
+	});
+};
+
 /* GET /quizes */
 exports.index = function(req, res) {
 	models.Quiz.findAll().then(function(quizes) {
 		res.render('quizes/index.ejs', {quizes: quizes});
+	}).catch(function(error){
+		next(error);
 	});
 };
 
 /* GET /quizes/:quizId */
 exports.show = function(req, res) {
-	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz : quiz});
-	});
+	res.render('quizes/show', {quiz : req.quiz});
 };
 
 /* GET /quizes/:quizId/answer */
 exports.answer = function(req, res) {
-	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		var rsp = "Incorrecto";
+	var rsp = "Incorrecto";
 		
-		if(req.query.respuesta === quiz.respuesta) {
-			rsp = "Correcto";
-		}
+	if(req.query.respuesta === req.quiz.respuesta) {
+		rsp = "Correcto";
+	}
 		
-		res.render('quizes/answer', {quiz: quiz, respuesta : rsp});
-	});
+	res.render('quizes/answer', {quiz: req.quiz, respuesta : rsp});
 };
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* Autoload de listas con :quizId*/
+router.param('quizId', quizController.load);
+
 router.get('/author', function(req, res) {
 	res.render('author', { title: 'Autor' });
 });
