6bb006dc081d680d2e49d16994d418ef661eb437
Javier Llinares <javier.jose.llinares@gmail.com>
2015-06-24 14:22:02 -0430

    Despliegue DB en Heroku
    Se suben los cambios necesarios para utilizar la BD Postgres de Heroku.

--- models/models.js
+++ models/models.js
@@ -1,18 +1,28 @@
 var path = require('path');
 
+/* Carga de parametros de conexion a la BD */
+var url 		= process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\/(.*)/);
+var DB_name 	= (url[6]||null);
+var user 		= (url[2]||null);
+var pwd 		= (url[3]||null);
+var protocol 	= (url[1]||null);
+var dialect 	= (url[1]||null);
+var port 		= (url[5]||null);
+var host 		= (url[4]||null);
+var storage 	= process.env.DATABASE_STORAGE;
+
 /* Carga Modelo ORM */
 var Sequelize = require('sequelize');
 
 /* Usar BD SQLite */
-var sequelize = new Sequelize(null, null, null, {dialect: "sqlite", storage: "quiz.sqlite"});
+var sequelize = new Sequelize(DB_name, user, pwd, {dialect: dialect, protocol: protocol, port: port, host: host, storage: storage, omitNull: true});
 
 /* Importar la definicion de la tabla Quiz en quiz.js */
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
 
 /* Exportar definicion de la tabla Quiz */
 exports.Quiz = Quiz;
-
-console.log(sequelize.sync());
 
 /* Se crea e inicializa la BD */
 sequelize.sync().then(function(){
